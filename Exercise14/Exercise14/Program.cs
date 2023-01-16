9c5b4a926baffb257e9c35e4522e44f6329416eb
Lyndon Panton <LyndonPanton@gmail.com>
2023-01-16 01:43:26 +0000

    Completed problem 2
    Added cards from deck to the hand
    Flipped over all the cards in the hand
    Printed information of all the cards in the hand

--- Exercise14/Exercise14/Program.cs
+++ Exercise14/Exercise14/Program.cs
@@ -11,6 +11,22 @@ namespace Exercise14
             List<Card> hand = new List<Card>();
 
             deck.Shuffle();
+
+            // problem 2
+            for (int i = 0; i < 5; i++)
+            {
+                hand.Add(deck.TakeTopCard());
+            }
+
+            foreach (Card card in hand)
+            {
+                card.FlipOver();
+            }
+
+            foreach(Card card in hand)
+            {
+                card.Print();
+            }
         }
     }
 }
